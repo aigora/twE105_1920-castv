75ad3d58592fd746aaeb4b963d569551d87f4a05
Simkamenov <61050066+Simkamenov@users.noreply.github.com>
2020-05-18 00:32:38 +0200

    18/05
    Retoques en el proceso de compra de peliculas. Imposibilitar la compra de peliculas adquiridas anteriormente

--- CasTV.c
+++ CasTV.c
@@ -41,7 +41,7 @@ typedef struct{//Estructura dia
 typedef struct{//Estructura película
 	char titulo[40];
 	tiempo t;
-	char tematica[20];
+	char tematica[25];
 	char year[5];
 }pelicula;
 
@@ -290,7 +290,7 @@ void detallespeliculas(pelicula p[],int i){//Funcion que despliega los detalles 
 int calculaprecio(pelicula p[],int i){//Atribuye un precio por la temática de la película
 	int precio,x;
 		if(strcmp(p[i-1].tematica,"accion")==0) x=1; if(strcmp(p[i-1].tematica,"animacion")==0) x=2; if(strcmp(p[i-1].tematica,"aventuras")==0) x=3;
-		if(strcmp(p[i-1].tematica,"historico")==0) x=4; if(strcmp(p[i-1].tematica,"terror")==0) x=5; if(strcmp(p[i-1].tematica,"ciencia ficcion")==0) x=6;
+		if(strcmp(p[i-1].tematica,"historico")==0) x=4; if(strcmp(p[i-1].tematica,"terror")==0) x=5; if(strcmp(p[i-1].tematica,"ciencia_ficcion")==0) x=6;
 		if(strcmp(p[i-1].tematica,"documental")==0) x=7; if(strcmp(p[i-1].tematica,"comedia")==0) x=8; if(strcmp(p[i-1].tematica,"musical")==0) x=9;
 		if(strcmp(p[i-1].tematica,"romantica")==0) x=10;
 	
@@ -451,8 +451,6 @@ int SelectorTematica(int nLineas, pelicula Pgratis[]){//Funcion para realizar un
 return CoincidenciaTotal;
 }
 
-
-
 int main (){//Programa principal
 
   EscribeNombre(); 
@@ -467,11 +465,12 @@ int main (){//Programa principal
 	  dia L[6], M[6], X[6], J[6], V[6], S[6], D[6];//Variables en las que se almacenarán los programas de cada día
 	  
 	  int op1=0, op2=0, op3=0, op4=0, op5=0, op6=0, op7=0, op8=0, op9=0, op10=0, op11=0, op12=0, //Operadores que utilizamos en bucles
-	  	  ContReg=0, i, i2, x1, x2, x3, x4,//Se utilizan en condicionales para el registro
+	  	  ContReg=0, i, j, i2, x1, x2, x3, x4,//Se utilizan en condicionales para el registro
 	  	  ContBanco=0,//Se utilizan en condicionales para el registro de la cuenta bancaria
 		  DiaActual=0,//Almacena numericamente el dia del sistema 
 		  NL[6], NM[6], NX[6], NJ[6], NV[6], NS[6], ND[6],//Numero de programas que tiene cada canal en cada dia
 		  NPC, NPP, NPG,//Almacenamos el numero de películas de cada tipo
+		  PeliculaComprada,//Se utiliza para asegurarse que una pelicula no se compra dos veces
 		  precio,//Guarda el precio de las peliculas
 		  Hora,Min,//Almacena la hora del sistema como enteros
 		  y, intentos, ingresosaldo,//Bucles para meter dinero
@@ -658,21 +657,23 @@ do{
 			    			
 			    		case 1: //Películas gratuitas
 			    		   
-			    	       printf("Estas son las peliculas por tematicas disponibles: \n");
+			    	       printf("Estas son todas las peliculas que disponemos. Si quiere realizar un filtrado dirijase al apartado recomendacion en el menu principal: \n\n");
 			          	   pelicomprada = fopen(RutaPeliculasGratuitas, "a");
 			          	   fclose(pelicomprada);
 			          	   CopiarFichero("peliculas/peliculas.txt",RutaPeliculasGratuitas);
 						   
 			    	       NPG=abrepeliculas(Pgratis,RutaPeliculasGratuitas);
-			    	       printf("Escriba el numero de una pelicula si quiere saber mas detalles.\n\n");
+			    	       
+			    	       printf("Pulse 1 si desea buscar alguna pelicula por el titulo o 2 si desea saber los detalles de alguna\n");
 					       do{
+					       	    printf("Escriba el numero de una pelicula si quiere saber mas detalles.\n\n");
 				    	    	scanf("%d",&op7);
 					  			if(op7>NPG || op7<0)
 				 	    			printf("Esa pelicula no existe. Pruebe con otro numero");
 					  	 	    else if(op7!=0) detallespeliculas(Pgratis,op7);	
 					  	 	    printf("\n\nSi desea volver al menu escriba menu.\n");
 					  	 	    printf("Si desea acabar el programa escriba salir.\n");
-					  	 	    printf("Y cualquier otra tecla para obtener mas detalles de otra pelicula\n");
+					  	 	    printf("O Z para obtener mas detalles de otra pelicula\n");
 					  	 	    scanf("%s",tecla2);
 						   }while(strcmp(tecla1,tecla2)<0);
 			    	       //Muestra el catálogo de películas entero con posibilidad de eleccion autónoma
@@ -700,11 +701,7 @@ do{
 			    	case 8:
 			    		abreSerie(s8,"juego de tronos/Temporada 8/8.txt");break;
 				}
-				do{
-				printf("\n\nSi desea volver al menu pulse cualquier tecla.\n");
-				printf("Si desea acabar el programa escriba salir.\n");
-				scanf("%s",tecla2);
-				}while(strcmp(tecla1,tecla2)<0);
+					retrocederprograma(tecla2);
 					break;
 			    }
 			    	break;
@@ -779,92 +776,130 @@ do{
 					  	 	    	printf("/////////////////////////////////////////////////////////////////////////////////////////////////////////// \n\n");
 					  	 	    	printf("Listado de peliculas que puede comprar: \n\n");
 					  	 	    	NPP=abrepeliculas(Ppago,"peliculas/peliculaspago.txt");
-					  	 	    	printf("Escriba el numero de una pelicula si quiere saber mas detalles, si quiere comprar alguna directamente escriba 0 \n\n");
-					  	 	    	do{
-					  	 	    		scanf("%d",&op7);
-					  	 	    		if(op7>NPP || op7<0)
-					  	 	    			printf("Esa pelicula no existe. Pruebe con otro numero o 0 si desea comprar alguna sin mirar sus detalles");
-					  	 	    		else if(op7!=0) detallespeliculas(Ppago,op7);	
-									}while(op7);
+					  	 	    	if(NPP==NPC){
+					  	 	    		printf("\n\nUsted ya ha adquirido todo el contenido de pago disponible.\n");
+					  	 	    		retrocederprograma(tecla2);
+									}
+					  	 	    	else {
+						  	 	    	printf("Escriba el numero de una pelicula si quiere saber mas detalles, si quiere comprar alguna directamente escriba 0 \n\n");
+						  	 	    	do{
+						  	 	    		scanf("%d",&op7);
+						  	 	    		if(op7>NPP || op7<0)
+						  	 	    			printf("Esa pelicula no existe. Pruebe con otro numero o 0 si desea comprar alguna sin mirar sus detalles");
+						  	 	    		else if(op7!=0) detallespeliculas(Ppago,op7);	
+										}while(op7);
+											
 										printf("Indique el numero de la pelicula que desea comprar: \n");
 										scanf("%d",&op7);
-									    precio=calculaprecio(Ppago,op7);
-									    
-									if(registro.x.saldo<precio){
-										printf("El saldo de su cuenta es insuficiente. No puede realizar la compra \n\n");
-										do{
-											printf("Si quiere probar con otra pelicula pulse 1\n");
-											printf("si quiere aumentar el saldo de su cuenta pulse 2\n");
-											printf("si quiere volver al menu pulse 3\n");
-											scanf("%d",&op8);
-											if(op8==1){
-												printf("Indique el numero de la pelicula que desea comprar: \n");
-												scanf("%d",&op7);
-									    		precio=calculaprecio(Ppago,op7);
-									    		if(registro.x.saldo>precio){
-													printf("Esta pelicula si esta a su alsance. Preparamos el proceso de compra \n\n");
-									    			registro.x.saldo=CompraPelicula(Ppago,op7,RutaPeliculasCompradas,RutaPeliculasGratuitas,registro.nombre,registro.x.saldo);
-												}
-											}
-											if(op8==2){
-												printf("Bienvenido al operador bancario: \n\n");
-												intentos=0;
-												do {
-				      							  if (intentos==3){
-				        				    	  printf("Las contrasenas no coinciden. Pulse 1 si desea recuperarla o cualquier tecla para acabar el programa. \n");
-				        						  scanf("%d",&y);
-				        	                    	if (y==1){
-				        		 						printf("Introduzca su DNI para verificar:");
-				        								scanf("%s",DNIusuario);
-				        								if(strcmp(DNIusuario,CompReg[i2-1].DNI)){
-				        									printf("Lo sentimos no podemos facilitarle la contrasena.");
-				        									exit(1);
+										
+										PeliculaComprada=0;	
+										for(j=0;j<NPC;j++){
+											if(strcmp(Ppago[op7-1].titulo,Pcompradas[j].titulo)==0)
+												PeliculaComprada++;
+										}	
+										
+										if(PeliculaComprada>0){
+											printf("\nEsta pelicula ya fue adquirida anteriormente\n");
+											retrocederprograma(tecla2);
+										}
+										else {
+											precio=calculaprecio(Ppago,op7);
+											if(registro.x.saldo<precio){
+												printf("El saldo de su cuenta es insuficiente. No puede realizar la compra \n\n");
+												do{
+													printf("Si quiere probar con otra pelicula pulse 1\n");
+													printf("si quiere aumentar el saldo de su cuenta pulse 2\n");
+													scanf("%d",&op8);
+													if(op8==1){
+														printf("Indique el numero de la pelicula que desea comprar: \n");
+														scanf("%d",&op7);
+														
+														PeliculaComprada=0;	
+														for(j=0;j<NPC;j++){
+															if(strcmp(Ppago[op7-1].titulo,Pcompradas[j].titulo)==0)
+																PeliculaComprada++;
 														}
-				        								else{
-				        									printf("La contrasena es: %s \n",CompRegBanco[i-1].x.pass);
-														}				        		
+														if(PeliculaComprada>0)
+															printf("\nEsta pelicula ya fue adquirida anteriormente\n");
+														else {
+												    		precio=calculaprecio(Ppago,op7);
+												    		if(registro.x.saldo>precio){
+																printf("Esta pelicula si esta a su alcance. Preparamos el proceso de compra \n\n");
+												    			registro.x.saldo=CompraPelicula(Ppago,op7,RutaPeliculasCompradas,RutaPeliculasGratuitas,registro.nombre,registro.x.saldo);
+												    			precio=-1;
+															}
+														}
 													}
-													else exit(1);
-				       							}
-				     						   	if (intentos>3)
-				        						printf("Le recuerdo que se contrasena es %s \n",CompRegBanco[i-1].x.pass);
-				        							printf("Introduzca la contrasena de la cuenta bajo el nombre de %s: ",registro.nombre);
-				       								 scanf("%s",ContrasenaBanco);
-				   	   							 intentos++;
-  												} while (strcmp(ContrasenaBanco,CompRegBanco[i-1].x.pass)!=0);
-  												printf("\n");
-  												printf("INGRESO MAXIMO 50 EUROS \n");
-  												printf("INGRESO MINIMO 10 EUROS \n\n");
-  												printf("Su saldo actual es: %d EUROS. Indique la cantidad a incorporar a su saldo: ",registro.x.saldo);
-  												do {
-												  scanf("%d",&ingresosaldo);
-												  if(ingresosaldo>50 || ingresosaldo<10)
-												  	printf("Cantidad no valida");
-												}while(ingresosaldo>50 || ingresosaldo<10);
-												registro.x.saldo+=ingresosaldo;
-												
-					  	 	    				saldobanco = fopen(RutaSaldo, "w");
-							    				fprintf(saldobanco,"%d",registro.x.saldo);
-										    	fclose(saldobanco);
-										    	
-										    	printf("Su nuevo saldo es: %d EUROS \n\n",registro.x.saldo);
-										    	if(registro.x.saldo>25){
-										    		printf("Ya dispone de suficiente saldo. Podemos pasar a realizar la compra de la pelicula. \n\n");
-										    		printf("inserte el numero de la pelicula que quiere comprar: ");
-										    		scanf("%d",&op7);
-												    precio=calculaprecio(Ppago,op7);
-												    registro.x.saldo=CompraPelicula(Ppago,op7,RutaPeliculasCompradas,RutaPeliculasGratuitas,registro.nombre,registro.x.saldo);    		
-												}	
-										     }
-										     if(op8==3)
-										     	precio=-1;
-										}while(registro.x.saldo<precio);
-										retrocederprograma(tecla2);
-									}
-									else{
-										printf("Pasamos a realziar la compra de la pelicula... \n\n");
-										registro.x.saldo=CompraPelicula(Ppago,op7,RutaPeliculasCompradas,RutaPeliculasGratuitas,registro.nombre,registro.x.saldo);
-										retrocederprograma(tecla2);
+													if(op8==2){
+														printf("Bienvenido al operador bancario: \n\n");
+														intentos=0;
+														do {
+						      							  if (intentos==3){
+						        				    	  printf("Las contrasenas no coinciden. Pulse 1 si desea recuperarla o cualquier tecla para acabar el programa. \n");
+						        						  scanf("%d",&y);
+						        	                    	if (y==1){
+						        		 						printf("Introduzca su DNI para verificar:");
+						        								scanf("%s",DNIusuario);
+						        								if(strcmp(DNIusuario,CompReg[i2-1].DNI)){
+						        									printf("Lo sentimos no podemos facilitarle la contrasena.");
+						        									exit(1);
+																}
+						        								else{
+						        									printf("La contrasena es: %s \n",CompRegBanco[i-1].x.pass);
+																}				        		
+															}
+															else exit(1);
+						       							}
+						     						   	if (intentos>3)
+						        						printf("Le recuerdo que se contrasena es %s \n",CompRegBanco[i-1].x.pass);
+						        							printf("Introduzca la contrasena de la cuenta bajo el nombre de %s: ",registro.nombre);
+						       								 scanf("%s",ContrasenaBanco);
+						   	   							 intentos++;
+		  												} while (strcmp(ContrasenaBanco,CompRegBanco[i-1].x.pass)!=0);
+		  												printf("\n");
+		  												printf("INGRESO MAXIMO 50 EUROS \n");
+		  												printf("INGRESO MINIMO 10 EUROS \n\n");
+		  												printf("Su saldo actual es: %d EUROS. Indique la cantidad a incorporar a su saldo: ",registro.x.saldo);
+		  												do {
+														  scanf("%d",&ingresosaldo);
+														  if(ingresosaldo>50 || ingresosaldo<10)
+														  	printf("Cantidad no valida");
+														}while(ingresosaldo>50 || ingresosaldo<10);
+														registro.x.saldo+=ingresosaldo;
+														
+							  	 	    				saldobanco = fopen(RutaSaldo, "w");
+									    				fprintf(saldobanco,"%d",registro.x.saldo);
+												    	fclose(saldobanco);
+												    	
+												    	printf("Su nuevo saldo es: %d EUROS \n\n",registro.x.saldo);
+												    	if(registro.x.saldo>25){
+												    		printf("Ya dispone de suficiente saldo. Podemos pasar a realizar la compra de la pelicula. \n\n");
+												    		printf("inserte el numero de la pelicula que quiere comprar: ");
+												    		scanf("%d",&op7);
+												    		
+												    		PeliculaComprada=0;	
+															for(j=0;j<NPC;j++){
+																if(strcmp(Ppago[op7-1].titulo,Pcompradas[j].titulo)==0)
+																	PeliculaComprada++;
+															}
+															if(PeliculaComprada>0)
+																printf("\nEsta pelicula ya fue adquirida anteriormente\n");
+															else{
+																precio=calculaprecio(Ppago,op7);
+															    registro.x.saldo=CompraPelicula(Ppago,op7,RutaPeliculasCompradas,RutaPeliculasGratuitas,registro.nombre,registro.x.saldo);
+																precio=-1;    
+															}		
+														}	
+												     }
+												}while(registro.x.saldo<precio);
+												retrocederprograma(tecla2);
+											}
+											else{
+												printf("Pasamos a realziar la compra de la pelicula... \n\n");
+												registro.x.saldo=CompraPelicula(Ppago,op7,RutaPeliculasCompradas,RutaPeliculasGratuitas,registro.nombre,registro.x.saldo);
+												retrocederprograma(tecla2);
+											}
+										}
 									}
 								}
 								else if (i>ContBanco){
@@ -1084,13 +1119,12 @@ do{
                                     case 0:
                                     	ProgramacionActual(D,ND,HoraSys,MinutoSys);
                                     break;
+								}
+							retrocederprograma(tecla2);
+							break;
 						}
-					
-					retrocederprograma(tecla2);
-			break;
+ 			break;
 		}
- break;
-	}
 	break;
 }
 }while(strcmp(tecla1,tecla2));
